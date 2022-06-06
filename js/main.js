4bee6718f35bb1d66898cf85a88c736a3b4e3cdc
Sherkhon <issakvell@gmail.com>
2022-06-06 14:11:07 +0500

    salom

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var firstName="Sherkhon"
+var watName="Bilmiyman"
+
+var firstName= prompt('Ismingizni kiriting' , firstName)
+
+var borderName= prompt('Border deb nimaga aytiladi?' , watName)
+
+var marginName= prompt('Margin nima uchun kerak?' , watName)
+
+var paddingName= prompt('Padding nima uchun kerak?' , watName)
+
+var borderName= prompt('Border nima uchun kerak?' , watName)
+
+var divName= prompt('Div nima?' , watName)
+
+console.log(firstName + '\n' + borderName + '\n' + marginName + '\n' + paddingName + '\n' + borderName + '\n' + divName);
